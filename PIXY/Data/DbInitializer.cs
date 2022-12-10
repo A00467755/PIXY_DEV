f0ea78c5503d2c073b2470f38b0763a09e5e158e
Chan Kin Wa <wa@test.com>
2022-12-10 12:08:24 -0400

    Remove assignment content (except model)

--- PIXY/Data/DbInitializer.cs
+++ PIXY/Data/DbInitializer.cs
@@ -101,65 +101,7 @@ namespace PIXY.Data
             }
 
             context.SaveChanges();
-            // Look for any students.
-            if (context.Students.Any())
-            {
-                return;   // DB has been seeded
-            }
 
-            var students = new Student[]
-            {
-            new Student{FirstName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01"),Anumber="A00460001"},
-            new Student{FirstName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01"),Anumber="A00460002"},
-            new Student{FirstName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01"),Anumber="A00460003"},
-            new Student{FirstName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01"),Anumber="A00460004"},
-            new Student{FirstName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01"),Anumber="A00460005"},
-            new Student{FirstName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01"),Anumber="A00460006"},
-            new Student{FirstName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01"),Anumber="A00460007"},
-            new Student{FirstName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01"),Anumber="A00460008"}
-            };
-            foreach (Student s in students)
-            {
-                context.Students.Add(s);
-            }
-            context.SaveChanges();
-
-            var courses = new Course[]
-            {
-            new Course{CourseID=1050,Title="Chemistry",Credits=3,CourseNumber=5510,Crn=17756},
-            new Course{CourseID=4022,Title="Microeconomics",Credits=3,CourseNumber=5520,Crn=17757},
-            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,CourseNumber=55310,Crn=17758},
-            new Course{CourseID=1045,Title="Calculus",Credits=4,CourseNumber=5540,Crn=17759},
-            new Course{CourseID=3141,Title="Trigonometry",Credits=4,CourseNumber=5550,Crn=17760},
-            new Course{CourseID=2021,Title="Composition",Credits=3,CourseNumber=5560,Crn=17761},
-            new Course{CourseID=2042,Title="Literature",Credits=4,CourseNumber=5570,Crn=17762}
-            };
-            foreach (Course c in courses)
-            {
-                context.Courses.Add(c);
-            }
-            context.SaveChanges();
-
-            var enrollments = new Enrollment[]
-            {
-            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
-            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
-            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
-            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
-            new Enrollment{StudentID=3,CourseID=1050},
-            new Enrollment{StudentID=4,CourseID=1050},
-            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
-            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
-            new Enrollment{StudentID=6,CourseID=1045},
-            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
-            };
-            foreach (Enrollment e in enrollments)
-            {
-                context.Enrollments.Add(e);
-            }
-            context.SaveChanges();
         }
     }
 }
--- PIXY/Data/PIXYContext.cs
+++ PIXY/Data/PIXYContext.cs
@@ -8,10 +8,6 @@ namespace PIXY.Data
         public PIXYContext(DbContextOptions<PIXYContext> options) : base(options)
         {
         }
-
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Student> Students { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Image> Images { get; set; }
         public DbSet<Cart> Carts { get; set; }
@@ -19,9 +15,6 @@ namespace PIXY.Data
         public DbSet<PurchasedItem> PurchasedItems { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<User>().ToTable("User");
             modelBuilder.Entity<Image>().ToTable("Image");
             modelBuilder.Entity<Cart>().ToTable("Cart");
