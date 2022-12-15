5c5653dd7f13ce01485b936e68fd2175eef6b723
Chan Kin Wa <wa@test.com>
2022-12-14 20:28:33 -0400

    merge with image_protection_and_zip

--- PIXY/Controllers/ImagesController.cs
+++ PIXY/Controllers/ImagesController.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PIXY.Data;
 using PIXY.Models;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace PIXY.Controllers
 {
@@ -53,7 +58,8 @@ namespace PIXY.Controllers
                                 Price = i.Price,
                                 HaveHardcopy = i.HaveHardcopy,
                                 IsPurchased = false,
-                                AuthorName = u.FirstName + " " + u.LastName
+                                AuthorName = u.FirstName + " " + u.LastName,
+                                ImageTags = i.ImageTags
                             };
 
                 //Apply Filter to Image List
@@ -94,7 +100,8 @@ namespace PIXY.Controllers
                                 HaveHardcopy = i.HaveHardcopy,
                                 IsPurchased = p == null ? false: true,
                                 IsInCart = c == null ? false : true,
-                                AuthorName = u.FirstName + " " + u.LastName
+                                AuthorName = u.FirstName + " " + u.LastName,
+                                ImageTags = i.ImageTags
                             };
 
                 //Apply Filter to Image List
@@ -113,6 +120,119 @@ namespace PIXY.Controllers
                 return View(imageCategoryVM);
             }
         }
+        
+        // Assign file path to purchased image
+  //      [Authorize]
+        [Route("images/protected/{FileName}")]
+        public IActionResult ShowProtectedImage(string FileName)
+        {
+            int ImageId = Int32.Parse(Left(FileName, 3));       // image id = frist 3 digit of file name
+
+            if (HttpContext.Session.GetInt32("UserID") != null && ImageId != 0)
+            {
+#pragma warning disable CS8629 // Nullable value type may be null.
+                int UserID = (int)HttpContext.Session.GetInt32("UserID");
+#pragma warning restore CS8629 // Nullable value type may be null.
+
+                // Check if the image is purchased by the user
+                var query = from p in _context.PurchasedItems
+                            join i in _context.Images on p.ImageId equals i.ID
+                            where p.UserId == UserID && i.ID == ImageId
+                            select new 
+                            {
+                                ID = p.ID,
+                                ImageId= i.ID
+                            };
+
+                var result = query.FirstOrDefault();
+
+                if (result != null)
+                {
+                    // Assign file path
+                    var file = Path.Combine(Directory.GetCurrentDirectory(),
+                                            "images_p", FileName);
+                    string ext = System.IO.Path.GetExtension(FileName).ToLower();
+                    Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+                    //get the mimetype of the file
+                    string mimeType = regKey.GetValue("Content Type").ToString();
+
+                    return PhysicalFile(file, mimeType);
+                }
+            }
+            // return new ForbidResult();       // Image is not purchased
+            return RedirectToAction("UnAuthorizedAccess", "Images");
+        }
+
+        public IActionResult UnAuthorizedAccess()
+        {
+            return View();
+        }
+
+        private string Left(string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            maxLength = Math.Abs(maxLength);
+
+            return (value.Length <= maxLength
+                   ? value
+                   : value.Substring(0, maxLength)
+                   );
+        }
+
+
+        public FileResult DownLoadZip(int ImageId)
+        {
+            var webRoot = Directory.GetCurrentDirectory();
+            var fileName = "PIXY_Images.zip";
+            var tempOutput = webRoot + "/Download/" + fileName;
+
+            using (ZipOutputStream IzipOutputStream = new ZipOutputStream(System.IO.File.Create(tempOutput)))
+            {
+                IzipOutputStream.SetLevel(9);
+                byte[] buffer = new byte[4096];
+                var imageList = new List<string>();
+
+                imageList.Add(Path.Combine(Directory.GetCurrentDirectory(),
+                        "images_p", ImageId.ToString().PadLeft(3, '0') + ".jpg"));
+                //imageList.Add(webRoot + "/Images/1data.png");
+
+
+                for (int i = 0; i < imageList.Count; i++)
+                {
+                    ZipEntry entry = new ZipEntry(Path.GetFileName(imageList[i]));
+                    entry.DateTime = DateTime.Now;
+                    entry.IsUnicodeText = true;
+                    IzipOutputStream.PutNextEntry(entry);
+
+                    using (FileStream oFileStream = System.IO.File.OpenRead(imageList[i]))
+                    {
+                        int sourceBytes;
+                        do
+                        {
+                            sourceBytes = oFileStream.Read(buffer, 0, buffer.Length);
+                            IzipOutputStream.Write(buffer, 0, sourceBytes);
+                        } while (sourceBytes > 0);
+                    }
+                }
+                IzipOutputStream.Finish();
+                IzipOutputStream.Flush();
+                IzipOutputStream.Close();
+            }
+
+            byte[] finalResult = System.IO.File.ReadAllBytes(tempOutput);
+            if (System.IO.File.Exists(tempOutput))
+            {
+                System.IO.File.Delete(tempOutput);
+            }
+            if (finalResult == null || !finalResult.Any())
+            {
+                throw new Exception(String.Format("Nothing found"));
+
+            }
+
+            return File(finalResult, "application/zip", fileName);
+        }
+
 
         private bool ImageExists(int id)
         {
