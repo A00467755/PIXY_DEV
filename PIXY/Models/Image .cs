d33a42808c4effa249613e29e01fe521dfdbb54e
Sharlene Wadhwa <sharlenewadhwa@gmail.com>
2022-12-14 21:22:04 -0400

    React component is added and Frontend is updated

--- PIXY/Models/Image .cs
+++ PIXY/Models/Image .cs
@@ -1,20 +1,30 @@
-﻿namespace PIXY.Models
+﻿using System.ComponentModel;
+
+namespace PIXY.Models
 {
     public class Image
     {
 
+       
         public int ID { get; set; }
 
+        [DisplayName("ID")]
         public int UserId { get; set; }
+
+        [DisplayName("CATEGORY")]
         public string CategoryDesc { get; set; }
 
+        [DisplayName("FORMAT")]
         public string ImageType { get; set; }
 
+        [DisplayName("PRODUCTS")]
         public string FilePathWatermark { get; set; }
         public string FilePath { get; set; }
 
+        [DisplayName("PRICE")]
         public Double Price { get; set; }
 
+        [DisplayName("FILE")]
         public Boolean HaveHardcopy { get; set; }
 
 
