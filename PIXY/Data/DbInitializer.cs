417512db01e06c02c285fad8b3104b3d172c8ebd
Chan Kin Wa <wa@test.com>
2022-12-16 13:14:57 -0400

    Config Dbinitializer for testing

--- PIXY/Data/DbInitializer.cs
+++ PIXY/Data/DbInitializer.cs
@@ -20,8 +20,8 @@ namespace PIXY.Data
 
             var users = new User[]
             {
-                 new User{FirstName="Carson",LastName="Alexander",Email="abc@abc.com",PhoneNumber="3232323",Address="adaddsafadfsdf",PostalCode="BH3-3C3",City="Halifax",Province="Nova Socotia",Country="Canada",UserName="AlexC",Password="dfsdfsd"},
-                 new User{FirstName="Tim",LastName="Hortans",Email="ttc@tim.com",PhoneNumber="3299323",Address="100 two two",PostalCode="12345-6789",City="New York City",Province="New York",Country="United State",UserName="tim",Password="dfsdfsd"}
+                 new User{FirstName="Carson",LastName="Alexander",Email="calex@hotmail.com",PhoneNumber="9024923240",Address="1075 Barrington St",PostalCode="B3H 2P8",City="Halifax",Province="Nova Scotia",Country="Canada",UserName="alexc",Password="123456"},
+                 new User{FirstName="Tim",LastName="Hortans",Email="ttc@gmail.com",PhoneNumber="9024229884",Address="1120 Queen St",PostalCode="B3H 2R9",City="Halifax",Province="Nova Scotia",Country="Canada",UserName="timho",Password="123456"}
            };
 
             foreach (User s in users)
@@ -38,10 +38,23 @@ namespace PIXY.Data
             
             var images = new Image[]
             {
-            new Image{ UserId =1,CategoryDesc="Natural",ImageType="jpg",ImageTags="green",FilePathWatermark="\\images_w\\001_w.jpg",FilePath="\\images\\protected\\001.jpg",Price=10.1,HaveHardcopy=true},
-            new Image{ UserId =1,CategoryDesc="Landscape",ImageType="jpg",ImageTags="nature,red",FilePathWatermark="\\images_w\\002_w.jpg",FilePath="\\images\\protected\\002.jpg",Price=10.1,HaveHardcopy=true},
-            new Image{ UserId =2,CategoryDesc="Food",ImageType="jpg",ImageTags="red",FilePathWatermark="\\images_w\\003_w.jpg",FilePath="\\images\\protected\\003.jpg",Price=10.1,HaveHardcopy=true},
-            new Image{ UserId =2,CategoryDesc="Landscape",ImageType="jpg",ImageTags="yello",FilePathWatermark="\\images_w\\004_w.jpg",FilePath="\\images\\protected\\004.jpg",Price=10.1,HaveHardcopy=true}
+            new Image{ UserId =1,CategoryDesc="Natural",ImageType="jpg",ImageTags="Green",FilePathWatermark="\\images_w\\001.jpg",FilePath="\\images\\protected\\001.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =1,CategoryDesc="Landscape",ImageType="jpg",ImageTags="Red",FilePathWatermark="\\images_w\\002.jpg",FilePath="\\images\\protected\\002.jpg",Price=12.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Food",ImageType="jpg",ImageTags="Curry,Yellow",FilePathWatermark="\\images_w\\003.jpg",FilePath="\\images\\protected\\003.jpg",Price=15.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Landscape",ImageType="jpg",ImageTags="Waterfront,Blue",FilePathWatermark="\\images_w\\004.jpg",FilePath="\\images\\protected\\004.jpg",Price=12.1,HaveHardcopy=true},
+            new Image{ UserId =1,CategoryDesc="Natural",ImageType="jpg",ImageTags="Insect,Yellow",FilePathWatermark="\\images_w\\005.jpg",FilePath="\\images\\protected\\005.jpg",Price=10.1,HaveHardcopy=true},
+            
+            new Image{ UserId =1,CategoryDesc="Landscape",ImageType="jpg",ImageTags="Waterfront,Blue,Green",FilePathWatermark="\\images_w\\006.jpg",FilePath="\\images\\protected\\006.jpg",Price=12.1,HaveHardcopy=true},
+            new Image{ UserId =1,CategoryDesc="Natural",ImageType="jpg",ImageTags="Flower,Plant,Green,White",FilePathWatermark="\\images_w\\007.jpg",FilePath="\\images\\protected\\007.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =1,CategoryDesc="Natural",ImageType="jpg",ImageTags="Flower,Plant,Green",FilePathWatermark="\\images_w\\008.jpg",FilePath="\\images\\protected\\008.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =1,CategoryDesc="Animal",ImageType="jpg",ImageTags="Cat,Yellow",FilePathWatermark="\\images_w\\009.jpg",FilePath="\\images\\protected\\009.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Animal",ImageType="jpg",ImageTags="Monkey,Green",FilePathWatermark="\\images_w\\010.jpg",FilePath="\\images\\protected\\010.jpg",Price=10.1,HaveHardcopy=true},
+            
+            new Image{ UserId =2,CategoryDesc="Landscape",ImageType="jpg",ImageTags="Waterfront,Blue",FilePathWatermark="\\images_w\\011.jpg",FilePath="\\images\\protected\\011.jpg",Price=12.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Street",ImageType="jpg",ImageTags="Food",FilePathWatermark="\\images_w\\012.jpg",FilePath="\\images\\protected\\012.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Street",ImageType="jpg",ImageTags="Festival,Xmas",FilePathWatermark="\\images_w\\013.jpg",FilePath="\\images\\protected\\013.jpg",Price=10.1,HaveHardcopy=true},
+            new Image{ UserId =2,CategoryDesc="Animal",ImageType="jpg",ImageTags="Carlton,Cat",FilePathWatermark="\\images_w\\014.jpg",FilePath="\\images\\protected\\014.jpg",Price=10.1,HaveHardcopy=true}
+
             };
 
             foreach (Image s in images)
@@ -59,7 +72,7 @@ namespace PIXY.Data
 
             var carts = new Cart[]
             {
-            new Cart{ UserId =1,ImageId=1,IsHardcopy=false,NoOfHardcopy=0}
+            new Cart{ UserId =1,ImageId=2,IsHardcopy=false,NoOfHardcopy=0}
             };
 
             foreach (Cart s in carts)
@@ -76,7 +89,7 @@ namespace PIXY.Data
 
             var transactions = new Transaction[]
             {
-                new Transaction{UserId =1,ImageId=2,IsHardcopy=false,NoOfHardcopy=0,PurchaseDataTime=DateTime.Parse("2005-09-01")}
+                new Transaction{UserId =1,ImageId=1,IsHardcopy=false,NoOfHardcopy=0,PurchaseDataTime=DateTime.Parse("2005-09-01")}
             };
 
             foreach (Transaction s in transactions)
